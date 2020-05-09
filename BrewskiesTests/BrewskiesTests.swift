ecec5a8875318e53440cc4fc0c0077f9ac2b5a1a
Vipul Delwadia <vipul.delwadia@gmail.com>
2020-05-10 08:57:20 +1200

    Initial Commit

---
+++ BrewskiesTests/BrewskiesTests.swift
@@ -0,0 +1,34 @@
+//
+//  BrewskiesTests.swift
+//  BrewskiesTests
+//
+//  Created by Vipul Delwadia on 10/05/20.
+//  Copyright © 2020 Vipul Delwadia. All rights reserved.
+//
+
+import XCTest
+@testable import Brewskies
+
+class BrewskiesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
